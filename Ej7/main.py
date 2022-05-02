5454125bd46ea2779e8c1c611bc59948b1b6aa3b
nicolahoz <103903534+nicolahoz@users.noreply.github.com>
2022-05-02 11:34:49 -0300

    Create main.py

---
+++ Ej7/main.py
@@ -0,0 +1,15 @@
+import os
+from Menu import Menu
+from Manejador import ManejadorViajero
+
+
+
+if __name__ == "__main__":
+    os.system("cls")
+    manViajero = ManejadorViajero()
+    manViajero.test()
+    manViajero.cargarViajero()
+
+    manViajero.compararSobrecargar()
+    manViajero.acumularSobrecargarReverso()
+    manViajero.canjearSobrecargaReverso()
